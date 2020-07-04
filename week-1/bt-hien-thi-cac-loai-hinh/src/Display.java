9806e8fbf7ecd150b59fec4f07f975e28872b62c
conaldev <talaaidoaibiet@gmail.com>
2020-07-04 16:47:25 +0700

    draw

--- week-1/bt-hien-thi-cac-loai-hinh/src/Display.java
+++ week-1/bt-hien-thi-cac-loai-hinh/src/Display.java
@@ -54,9 +54,10 @@ public class Display {
                     break;
                 case 3:
                     for(int i=1;i<=5;i++) {
-                        for(int j=1;j<=5;j++) {
-                            System.out.printf("* ");
-                        }
+                        for(int j=4;j>=i;j--)
+                            System.out.printf("  ");
+                        for(int k=1;k<=2*i-1;k++)
+                            System.out.printf("* ");;
                         System.out.println();
                     }
                     break;
