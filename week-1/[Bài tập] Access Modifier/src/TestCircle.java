4f8fe582c6d472c2b54d7bd950b9e8bfcc3361f2
conaldev <talaaidoaibiet@gmail.com>
2020-07-07 18:44:57 +0700

    bai tap access modifier

---
+++ week-1/[Bài tập] Access Modifier/src/TestCircle.java
@@ -0,0 +1,7 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle myCircle = new Circle();
+        System.out.println("Radius = " + myCircle.getRadius());
+        System.out.println("Area = " + myCircle.getArea());
+    }
+}
