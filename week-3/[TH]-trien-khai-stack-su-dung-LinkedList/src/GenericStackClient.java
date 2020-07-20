566b0a4b4a84cc7236180598f8b202a98ba4106e
conaldev <talaaidoaibiet@gmail.com>
2020-07-20 11:42:51 +0700

    bai tap

---
+++ week-3/[TH]-trien-khai-stack-su-dung-LinkedList/src/GenericStackClient.java
@@ -0,0 +1,8 @@
+public class GenericStackClient {
+    public static void main(String[] args) {
+        System.out.println("\n1. Stack of Strings");
+        MyGenericStack.stackOfStrings();
+        System.out.println("2. Stack of integers");
+        MyGenericStack.stackOfIntegers();
+    }
+}
