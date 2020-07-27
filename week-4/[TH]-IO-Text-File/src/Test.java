89e5ac27a62bddf9f745abb6e8bf423e1dbd59bc
conaldev <talaaidoaibiet@gmail.com>
2020-07-27 10:07:31 +0700

    thuc hanh IO text file

---
+++ week-4/[TH]-IO-Text-File/src/Test.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Nhập đường dẫn file: ");
+        Scanner scanner = new Scanner(System.in);
+        String path = scanner.nextLine();
+
+        ReadFileExample readfileEx = new ReadFileExample();
+        readfileEx.readFileText(path);
+    }
+}
