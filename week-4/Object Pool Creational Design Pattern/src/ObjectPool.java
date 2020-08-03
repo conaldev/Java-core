610a3b2f26cc7afcbcca7e2db68d4125e11cbbc6
conaldev <talaaidoaibiet@gmail.com>
2020-08-03 14:10:46 +0700

    >>>

--- week-4/Object Pool Creational Design Pattern/src/ObjectPool.java
+++ week-4/Object Pool Creational Design Pattern/src/ObjectPool.java
@@ -4,12 +4,12 @@ import java.util.Hashtable;
 public abstract class ObjectPool<T> {
     private long expirationTime;
 
-    private Hashtable<T, long> locked, unlocked;
+    private Hashtable<T, Long> locked, unlocked;
 
     public ObjectPool() {
         expirationTime = 30000;
-        locked = new Hashtable<T, long>();
-        unlocked = new Hashtable<T, long>();
+        locked = new Hashtable<T, Long>();
+        unlocked = new Hashtable<T, Long>();
     }
 
     protected abstract T creat();
