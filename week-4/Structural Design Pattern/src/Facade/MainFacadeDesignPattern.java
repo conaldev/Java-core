41d37c4649d9617760aacf0e54f35e76f25ec674
conaldev <talaaidoaibiet@gmail.com>
2020-08-04 22:03:04 +0700

    design pattern Facade

---
+++ week-4/Structural Design Pattern/src/Facade/MainFacadeDesignPattern.java
@@ -0,0 +1,8 @@
+package Facade;
+
+public class MainFacadeDesignPattern {
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.share("This is a post");
+    }
+}
